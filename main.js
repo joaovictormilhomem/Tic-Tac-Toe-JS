e295c4bcac2a9a2b8b9c447fd4200ddb96b3bee0
João Victor Rocha Milhomem <joaovictormilho@gmail.com>
2021-02-20 12:05:23 -0300

    check winner

--- main.js
+++ main.js
@@ -5,25 +5,38 @@ startGame();
 
 function startGame() {
     let squares = getSquares();
-    setOnclickOnSquares(squares);
+    setOnclickOnSquares(squares, false);
 }
 
 function getSquares() {
     return document.getElementsByClassName('square');
 }
 
-function setOnclickOnSquares(squares) {
-    for(const square of squares) {
-        square.onclick = () => {
-            squareIsClicked(square);
+function setOnclickOnSquares(squares, isOver) {
+    if (isOver)
+        for(const square of squares) {
+            square.onclick = () => {
+                
+            }
         }
-    }
+    else
+        for(const square of squares) {
+            square.onclick = () => {
+                squareIsClicked(square);
+            }
+        }
 }
 
 function squareIsClicked(square) {
-    if (checkIfIsNotMarked(square)) {
+    if (isNotMarked(square)) {
         markSquare(square);
-        changePlayerPlaying();
+        if (!gameIsOver()) {   
+            changePlayerPlaying();
+        }
+        else{
+            let squares = getSquares();
+            setOnclickOnSquares(squares, true);
+        }
     }
 }
 
@@ -32,7 +45,7 @@ function markSquare(square) {
     stageCopy[parseInt(square.id[6])] = playerPlaying;
 }
 
-function checkIfIsNotMarked(square) {
+function isNotMarked(square) {
     if(square.innerHTML === '' && stageCopy[parseInt(square.id[6])] === null)
         return true;
     else
@@ -40,18 +53,49 @@ function checkIfIsNotMarked(square) {
 }
 
 function changePlayerPlaying() {
-    if (playerPlaying === 'X')
+    
+
+    if (playerPlaying === 'X'){
         playerPlaying = 'O';
-    else
+        setTitle('Jogador O');
+    }
+    else{
         playerPlaying = 'X';
+        setTitle('Jogador X');
+    }
 }
 
-function checkGameOver() {
-    let squares = getSquares();
+function setTitle(text) {
+    let title = document.getElementById('title');
+    title.innerHTML = text;
+}
 
-    for(const square of squares) {
-        
+function gameIsDraw(currentValue) {
+    return currentValue !== null;
+}
+
+function playerPlayingWins(one,two,three) {
+    if(stageCopy[one] === playerPlaying && stageCopy[two] === playerPlaying && stageCopy[three] === playerPlaying)
+        return true;
+}
+
+function gameIsOver() {
+
+    if (playerPlayingWins(0,1,2) || playerPlayingWins(3,4,5) || playerPlayingWins(6,7,8) || playerPlayingWins(0,3,6) ||
+        playerPlayingWins(1,4,7) || playerPlayingWins(2,5,8) || playerPlayingWins(0,4,8) || playerPlayingWins(2,4,6))
+    {
+        setTitle(`${playerPlaying} venceu`);
+        playerPlaying = null;
+        return true;
     }
+
+    else if (stageCopy.every(gameIsDraw)) {
+        setTitle('deu velha');
+        playerPlaying = null;
+        return true;
+    }
+
+    return false;
 }
 
 /* 
